b5facdd3597c43c2464f78daa02d1a070ce0f0f0
Sardorbek Isakulov <sardorbekisakulov720@gmail.com>
2024-04-19 12:35:22 +0500

    third

---
+++ app_main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home_page, name='home'),
+    path('teachers/', views.teachers, name='teachers'),
+    path('teacher/<int:id>/', views.teacher_detail, name='teacher_detail'),
+    path('teacher/delete/<int:id>/', views.teacher_delete, name='teacher_delete'),
+
+    path('teachers/new/', views.teacher_create, name='teacher_create'),
+    path('teachers/update/<int:id>/', views.teacher_update, name='teacher_update')
+]
---
+++ app_main/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model
+from django.http import Http404
+
+User = get_user_model()
+
+
+def home_page(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    full_name = request.user.get_full_name()
+    context = {
+        "full_name": full_name,
+    }
+
+    return render(request, 'app_main/home.html', context)
+
+
+def teachers(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    if not request.user.is_staff and not request.user.is_superuser:
+        return redirect('home')
+
+    teachers_list = User.objects.all()
+
+    context = {
+        'teachers': teachers_list
+    }
+
+    return render(request, 'app_main/teachers.html', context)
+
+
+def teacher_create(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        if first_name and last_name and username and email and password1 and password2:
+            if password1 == password2:
+                user = User.objects.create(
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    username=username,
+                )
+                user.set_password(password2)
+                user.save()
+                return redirect('teachers')
+
+    return render(request, 'app_main/teacher_form.html')
+
+
+def teacher_detail(request, id):
+    teacher = get_object_or_404(User, id=id)
+    context = {
+        'teacher': teacher
+    }
+    return render(request, 'app_main/teacher.html', context)
+
+
+def teacher_update(request, id):
+    teacher = get_object_or_404(User, id=id)
+
+    if request.method == 'POST':
+
+        teacher.first_name = request.POST.get('first_name', teacher.first_name)
+        teacher.last_name = request.POST.get('last_name', teacher.last_name)
+
+        teacher.save()
+        return redirect('teacher_detail', id=id)
+
+    context = {
+        'teacher': teacher
+    }
+    return render(request, 'app_main/teacher_form.html', context)
+
+
+def teacher_delete(request, id):
+    user = get_object_or_404(User, id=id)
+    user.delete()
+    return redirect('teachers')
